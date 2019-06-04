b21bb2eab0872fa52cb217023aa1f5fb7d7c04e8
Andriusjok <andriusjokubauskass@gmail.com>
2019-06-04 13:49:57 +0300

    start

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <sstream>
 #include <regex>
+#include <iomanip>
 using std::cout;
 using std::endl;
 using std::cin;
@@ -15,13 +16,14 @@ using std::ofstream;
 using std::stringstream;
 using std::multimap;
 using std::vector;
+using std::setw;
 bool Url(const string &word) {
 	std::regex rgx("^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?");
 	return std::regex_match(word, rgx);
 }
 bool specialchar(char & a)
 {
-	if (std::isalpha(a) or std::isdigit(a)) {
+	if (std::isalpha(a) or std::isdigit(a) or a=="ą" or a=="č" or a=="ę" or a=="ė" or a=="į" or a=="š" or a=="ų" or a=="ū") {
 		return false;
 	} return true;
 }
@@ -48,7 +50,6 @@ int main ()
 		while (eil >> oneword) {
 			std::transform(oneword.begin(), oneword.end(), oneword.begin(), ::tolower);
 			if (Url(oneword)) {
-				WordCounter.insert({ oneword, linenr });
 				links.push_back(oneword);
 			} else {
 				oneword.erase(std::remove_if(oneword.begin(), oneword.end(), specialchar), oneword.end());
@@ -60,17 +61,38 @@ int main ()
 	}
 	file.close();
 	ofstream ofile("rez" + pav);
+	auto  maxCount=1;
+	unsigned int maxWord=1;
+		for (auto it = WordCounter.begin(), end = WordCounter.end(); it != end; it = WordCounter.upper_bound(it->first))
+	{
+		if (maxCount<WordCounter.count(it->first))
+			maxCount=WordCounter.count(it->first);
+		if (maxWord<it->first.length())
+			maxWord=it->first.length();
+
+	}
+
+	ofile<<setw(maxWord+1)<<std::left<<"Zodis"<<setw(21)<<"pakartojimu skaicius"<<" "<<"eilutese"<<endl;	
+	for(auto a=2;a<=maxCount;a++){
 	for (auto it = WordCounter.begin(), end = WordCounter.end(); it != end; it = WordCounter.upper_bound(it->first))
 	{
-		if (WordCounter.count(it->first) > 1)
+
+		if (WordCounter.count(it->first) ==a)
 		{
-			ofile << " " << it->first << " pasikartojo " << WordCounter.count(it->first) << " kartus eilutese:";
+			
+			ofile<<setw(maxWord+1)<<std::left<< it->first <<setw(21)<<std::left<<WordCounter.count(it->first);
 			for (auto itline = WordCounter.equal_range(it->first).first; itline != WordCounter.equal_range(it->first).second; ++itline) {
+				auto o = itline;
+				o--;
+				if(o->second!=itline->second)
 				ofile << ' ' << itline->second;
 			}
 			ofile << endl;
+		
 		}
+
 	}
+}
 	ofile << "URLs: " << endl;
 	for (auto it = links.begin(); it != links.end(); it++)
 	{
